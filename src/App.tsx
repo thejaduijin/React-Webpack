5d4adee59eca9212a5a2be41ed3c4a8aeec025b3
thejaduijin <lokendra.06.work@gmail.com>
2022-11-18 17:38:56 +0530

    husky test done

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import BOMB from '../public/ultra_bomb.png'
 import { Counter } from './Counter'
 
 function App() {
-  const name = 'Lokendra';
+  const first = 'Lokendra'
+  const last = 'Lokendra'
   return (
     <>
       <h1>
-        Hi I'm {name}  React Webpack - {process.env.NODE_ENV} {process.env.name}
+        Hi Im {first} {last} React Webpack - {process.env.NODE_ENV}{' '}
+        {process.env.name}
       </h1>
       <img src={BOMB} alt="logo" width="300" height="200" />
       <Counter />
