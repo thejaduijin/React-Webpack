88e6aca67dad70545eb125b99c5f6864ca628984
thejaduijin <lokendra.06.work@gmail.com>
2022-11-18 17:35:10 +0530

    code formatting added

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <div>
       <h3>Update the count and edit src/App.tsx, state is preserved</h3>
